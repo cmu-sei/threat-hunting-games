73769cd77949176250364843a31b96891d75d873
Matt Sisk <sisk@cert.org>
2023-01-10 13:02:11 -0500

    add support for top level imports of games

--- threat_hunting_games/gameload.py
+++ threat_hunting_games/gameload.py
@@ -1,7 +1,13 @@
-from . import v0
-from . import v1
-from . import v2
-from . import v2_seq
+try:
+    from . import v0
+    from . import v1
+    from . import v2
+    from . import v2_seq
+except ImportError:
+    import v0
+    import v1
+    import v2
+    import v2_seq
 
 #current_game = v2
 current_game = v2_seq

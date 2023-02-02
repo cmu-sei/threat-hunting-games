76df4f5ade1184d380a68754a3414fcf0072ceae
Matt Sisk <sisk@cert.org>
2023-02-02 13:53:25 -0500

    cleaned up debugs in gameload

--- threat_hunting_games/gameload.py
+++ threat_hunting_games/gameload.py
@@ -1,6 +1,5 @@
 try:
     # for scripts living in subdirs, e.g. ./threat_hunting_games/examples
-    print("trying .games")
     from .games import v0
     from .games import v1
     from .games import v2
@@ -9,7 +8,6 @@ try:
     from .games import v2_matrix
 except (ModuleNotFoundError, ImportError):
     # for scripts living in top level ./threat_hunting_games
-    print("trying games")
     from games import v0
     from games import v1
     from games import v2
@@ -17,11 +15,9 @@ except (ModuleNotFoundError, ImportError):
     from games import v2_tensormod
     from games import v2_matrix
 
-print("imports complete")
-
-#current_game = v2
+current_game = v2
 #current_game = v2_seq
 #current_game = v2_tensormod
-current_game = v2_matrix
+#current_game = v2_matrix
 
 game_name = current_game.game_name

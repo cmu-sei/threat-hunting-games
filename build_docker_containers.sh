87d39de2c9e1a35d4c7e54b7e88fc2cce4cfb16b
Jared Frank <jfrank@cert.org>
2023-02-09 12:08:21 -0500

    Fixed build_containers script to use arguments better, moved GHOSTS-SPECTRE into Containers folder, added in grafana files

--- build_docker_containers.sh
+++ build_docker_containers.sh
@@ -1,28 +1,46 @@
 #!/bin/sh
-if [ "$3" = "--reset" ]
+RESET=0
+DETACH=0
+VPN=0
+export COMPOSE_PROJECT_NAME=threat-hunting-games
+usage() {
+  echo "Usage: $0 [ -d Run in Detached Mode ] [ -r Reset the environment data in GHOSTS ] [ -v Build the VPN configuration of the containers] " 1>&2
+}
+while getopts "drv" arg; do
+  case $arg in
+    d) DETACH="1" ;;
+    r) RESET=1 ;;
+    v) VPN="1" ;;
+    *) echo 'error' >&2
+      exit 1
+  esac
+done
+
+if [ $RESET -eq 1 ]
 then
-  rm GHOSTS_Environment/Environment_Data/*
-  touch GHOSTS_Environment/Environment_Data/.keep
+  echo "RESETTING ENVIRONMENT DATA..."
+  rm -rf Containers/Environment_Data/db_data
+  rm -rf Containers/Environment_Data/g_data
+  rm -rf Containers/Environment_Data/spectre_data
+echo "ALL ENVIRONMENT DATA RESET..."
+echo " "
 fi
-if [ "$1" = "vpn" ]
+if [ $VPN -eq 1 ]
+echo "BUILDING VPN VERSION OF CONTAINER..."
 then
-  if [ "$3" = "-d" ]
+  if [ $DETACH -eq 1 ]
   then
-    docker compose --file Docker_Container/docker-compose.vpn.yml up -d
+    docker compose --file Containers/docker-compose.vpn.yml up -d --force--recreate
     docker ps
   else
-    docker compose --file Docker_Container/docker-compose.vpn.yml up
-  fi
-elif [ "$1" = "non-vpn" ]
-then
-  if [ "$3" = "-d" ]
-  then
-    docker compose --file Docker_Container/docker-compose.yml up -d
-    docker ps
-  else
-    docker compose --file Docker_Container/docker-compose.yml up
+    docker compose --file Containers/docker-compose.vpn.yml up --force-recreate
   fi
 else
-  echo "Please provide argument after script call: ['vpn', 'non-vpn']"
-  exit 1
+  if [ $DETACH -eq 1 ]
+  then
+    docker compose --file Containers/docker-compose.yml up -d --force-recreate
+    docker ps
+  else
+    docker compose --file Containers/docker-compose.yml up --force-recreate
+  fi
 fi

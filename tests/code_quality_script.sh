0583a8c497581943fd587fcafc44352364aa5c06
Jared Frank <jfrank@cert.org>
2023-02-13 16:36:46 -0500

    Fixing incorrect folder in code_quality_script.sh

--- tests/code_quality_script.sh
+++ tests/code_quality_script.sh
@@ -1,5 +1,5 @@
 # Command for running pytest on codebase
-/opt/pysetup/.venv/bin/py.test /app
+/opt/pysetup/.venv/bin/py.test /src
 
 # Command for running mypy on codebase
 /opt/pysetup/.venv/bin/mypy ../threat_hunting_game
